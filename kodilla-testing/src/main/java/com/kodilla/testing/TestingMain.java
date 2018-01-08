bdf6b45d957ae3a0d4cf24ac1a1e24a8e16981c4
Sebastian Krezel <s.krezel@excellbit.pl>
2018-01-08 23:06:00 +0100

    Calculator test first Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        Calculator calTest = new Calculator(6, 4);
+
+        Integer addResult = calTest.add();
+        Integer subtractResult = calTest.subtract();
+
+        if (addResult.equals(6 + 4)) {
+            System.out.println("Aadding test is correct");
+        } else {
+            System.out.println("Adding generate ERRORS!!!");
+        }
+
+        if (subtractResult.equals(6 - 4)) {
+            System.out.println("Subtracting test is correct");
+        } else {
+            System.out.println("Subtracting generate ERRORS!!!");
+        }
     }
 }
