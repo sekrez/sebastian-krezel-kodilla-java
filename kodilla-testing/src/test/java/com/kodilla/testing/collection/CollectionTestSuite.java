c339351c64ee7df7bece668622235a9d6d74ce43
Sebastian Krezel <s.krezel@excellbit.pl>
2018-02-03 22:55:52 +0100

    First update for tests

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,10 +1,12 @@
 // Zadanie 6.3
 
 package com.kodilla.testing.collection;
-import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Random;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 
 public class CollectionTestSuite {
 
@@ -19,22 +21,30 @@ public class CollectionTestSuite {
         System.out.println("Test Suite: end");
     }
 
-        @Test
+    @Test
     public void testOddNumbersExterminatorEmptyList() {
-            //Given
-            OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-            ArrayList<Integer> emptyList = new ArrayList<>();
+        //Given
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> emptyList = new ArrayList<>();
+        ByteArrayOutputStream redirectMessage = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(redirectMessage);
+        PrintStream old = System.out;
 
-            //When
-            System.out.println("Testing for empty ArrayList");
+        //When
+        System.setOut(ps);
+        ArrayList<Integer> testaArray = exterminator.exterminate(emptyList);
+        String result = redirectMessage.toString();
+        System.out.flush();
+        System.setOut(old);
 
-            //Then
-            ArrayList<Integer> firstTest = oddNumbersExterminator.exterminate(emptyList);
-        }
+        //Then
+        System.out.println("Testing exception for empty ArrayList.");
+        Assert.assertEquals(result, "ArrayList is empty. Abort checking list.\n");
+    }
 
 
     @Test
-    public void testOddNumbersExterminatorNormalList(){
+    public void testOddNumbersExterminatorNormalList() {
         //Given
         MakerSampleList testList = new MakerSampleList();
         ArrayList<Integer> genTestList = testList.generateList();
@@ -45,16 +55,10 @@ public class CollectionTestSuite {
 
         //Then
         int z = 0;
-         System.out.print("Testing sample array: ");
-        for (int i : result){
-            System.out.print(i + ", ");
-            z = z + (i%2);
+        System.out.print("Testing sample array!");
+        for (int i : result) {
+            z = z + (i % 2);
         }
-        System.out.print("\b");
-        Assert.assertEquals(0,z);
+        Assert.assertFalse(z != 0);
     }
-
-
-
-
 }
